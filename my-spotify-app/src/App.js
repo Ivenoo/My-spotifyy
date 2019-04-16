7cb9e2faebd81f6876ab67b219cfb0c33b56c701
Ivenoo <32682666+Ivenoo@users.noreply.github.com>
2019-04-16 13:54:19 +0200

    all of them

--- my-spotify-app/src/App.js
+++ my-spotify-app/src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-      
+      <input className="input-search" type="text" placeholder="Search..."></input>
       </div>
     );
   }
