239902296df329b564da10be0b82b8979774df7b
lhoro <32704441+lhoro@users.noreply.github.com>
2019-06-06 19:32:27 +0200

    Updates

---
+++ precache-manifest.c483d21fe788daa36208045c1a51c7c4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "220ffbdf58d311898c99",
+    "url": "/My-spotify/my-spotify-app/static/js/runtime~main.75dd321a.js"
+  },
+  {
+    "revision": "b7268802c20fab4383d3",
+    "url": "/My-spotify/my-spotify-app/static/js/main.d9f25105.chunk.js"
+  },
+  {
+    "revision": "0de520a237594fb7f637",
+    "url": "/My-spotify/my-spotify-app/static/js/2.cf67ae08.chunk.js"
+  },
+  {
+    "revision": "6949b22120fa714d76d5a31c09a5aec1",
+    "url": "/My-spotify/my-spotify-app/index.html"
+  }
+];
